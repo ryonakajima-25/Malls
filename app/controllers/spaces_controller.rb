ae2d1c8cb1fd8495a1cba036c069c95b5186d718
ryonakajima-25 <nov.5x5@gmail.com>
2019-12-20 17:44:56 +0900

    edit

--- app/controllers/spaces_controller.rb
+++ app/controllers/spaces_controller.rb
@@ -2,7 +2,7 @@ class SpacesController < ApplicationController
   # before_action :authenticate_user!
 
   def index
-    @spaces = Space.all 
+    @spaces = Space.all.order("updated_at DESC")
   end
 
   def new
@@ -15,10 +15,32 @@ class SpacesController < ApplicationController
     redirect_to '/'
   end
 
+  def destroy
+    space = Space.find(params[:id])
+    space.destroy
+  end
+
+  def edit
+    set_space
+  end
+
+  def update
+    space = Space.find(params[:id])
+    space.update(space_params)
+  end
+
+  def show
+    set_space
+  end
+
 
   private
   def space_params
     params.require(:space).permit(:location, :mall_name, :floor, :block_number, :area, :rent, :sector, :image).merge(developer_id: current_developer.id)
+  end
+
+  def set_space
+    @space = Space.find(params[:id])
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :developers
   root "spaces#index"
-  resources :spaces, only: [:index, :new, :create]
+  resources :spaces
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
