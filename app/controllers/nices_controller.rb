736fa4c7e526032fbe2f218a62952daeb61c2e66
ryonakajima-25 <nov.5x5@gmail.com>
2019-12-24 19:25:42 +0900

    edit

--- app/controllers/nices_controller.rb
+++ app/controllers/nices_controller.rb
@@ -1,13 +1,13 @@
 class NicesController < ApplicationController
   def create
     @nice = current_tenant.nices.create(space_id: params[:space_id])
-    redirect_to spaces_path
+    redirect_to space_path(@nice.space_id)
   end
 
   def destroy
     @nice = Nice.find_by(space_id: params[:space_id], tenant_id: current_tenant.id)
     @nice.destroy
-    redirect_to spaces_path
+    redirect_to space_path(@nice.space_id)
   end
 end
 
