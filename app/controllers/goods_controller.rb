ae55ace56a11510363764108d67e20050702b0f4
ryonakajima-25 <nov.5x5@gmail.com>
2020-01-05 11:26:37 +0900

    edit

--- app/controllers/goods_controller.rb
+++ app/controllers/goods_controller.rb
@@ -3,21 +3,21 @@ class GoodsController < ApplicationController
 
   def create
     @good = Good.create(developer_id: current_developer.id, brand_id: params[:brand_id])
-    @goods = Good.where(brand_id: params[:brand_id])
-    @brand.reload
+    @goods = Good.where(brand_id: @brand.id)
+    @brands = Brand.all
   end
 
   def destroy
     good = Good.find_by(developer_id: current_developer.id, brand_id: params[:brand_id])
     good.destroy
-    @goods = Good.where(brand_id: params[:brand_id])
-    @brand.reload
+    @goods = Good.where(brand_id: @brand.id)
+    @brands = Brand.all
   end
 
   private
   def set_brand
     @brand = Brand.find(params[:brand_id])
   end
-
 end
 
+# -----------------■■■ goodの非同期化にトライしたが、不可だった(1/5) ■■■------------------------------
