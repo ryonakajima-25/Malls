48473efc12eab9443d3b3da6753bf16fc38eea68
ryonakajima-25 <nov.5x5@gmail.com>
2020-01-06 19:57:20 +0900

    edit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,14 +2,14 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-  def after_sign_in_path_for(developer)
-    brands_path
+  def after_sign_in_path_for(resource)
+    if developer_signed_in?
+      brands_path
+    elsif tenant_signed_in?
+      spaces_path
+    end
   end
   
-  def after_sign_in_path_for(tenant)
-    spaces_path
-  end
-
   protected
 
   def configure_permitted_parameters
