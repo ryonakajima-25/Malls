ca804602bd7b31406ee1b42fe3e9a1fc60c2ce1c
ryonakajima-25 <nov.5x5@gmail.com>
2020-01-06 11:17:25 +0900

    edit

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -3,7 +3,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   # include CarrierWave::RMagick
   include CarrierWave::MiniMagick
 
-  process resize_to_fit: [250, 250]
+  process resize_to_fit: [250, 300]
 
   # Choose what kind of storage to use for this uploader:
   storage :file
