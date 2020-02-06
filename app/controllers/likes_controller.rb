e40e555c1021cd5c5b684f95e8060ce374669833
ryonakajima-25 <nov.5x5@gmail.com>
2020-02-06 10:42:13 +0900

    同期通信いいね完了

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,8 +1,6 @@
 class LikesController < ApplicationController
-  # before_action :set_space, only: [:create, :destroy]
-
+  
   def create
-    # binding.pry
     if params[:space_id]
       @like = current_user.likes.create(space_id: params[:space_id])
       redirect_to space_path(@space)
@@ -23,14 +21,4 @@ class LikesController < ApplicationController
       redirect_to brand_path(@brand)
     end
   end
-
-  private
-  # def set_target
-  #   binding.pry
-  #   if params[:space_id]
-  #     @space = Space.find(params[:space_id])
-  #   elsif params[:brand_id]
-  #     @brand = Brand.find(params[:brand_id])
-  #   end
-  # end
 end
