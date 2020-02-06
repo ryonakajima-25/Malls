dbbdc38bf822b65effe658048056a4c90930e0a9
ryonakajima-25 <nov.5x5@gmail.com>
2020-02-06 13:00:28 +0900

    いいね整理

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,8 @@ class User < ApplicationRecord
   has_many :spaces
   has_many :brands
   has_many :likes, dependent: :destroy
+  has_many :liked_spaces, through: :likes, source: :space
+  has_many :liked_brands, through: :likes, source: :brand
 
   enum genre: { 
     developer:  1,  # 商業施設
