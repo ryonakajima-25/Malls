8e93b156647e343505a36517c7a289e3aec3934a
ryonakajima-25 <nov.5x5@gmail.com>
2020-02-10 20:45:38 +0900

    メッセージ機能実装

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -1,15 +1,21 @@
 class ConversationsController < ApplicationController
   before_action :authenticate_user!
+
+  def index
+    @user = User.find(current_user.id)
+    @entries = Entry.where(user_id: current_user.id)
+  end
+
   def create
     @conversation = Conversation.create
-    @entry1 = Entry.create(:conversation_id => @conversation.id, :user_id => current_user.id)
-    @entry2 = Entry.create(params.require(:entry).permit(:user_id, :conversation_id).merge(:conversation_id => @conversation.id))
+    @entry1 = Entry.create(conversation_id: @conversation.id, user_id: current_user.id)
+    @entry2 = Entry.create(params.require(:entry).permit(:user_id, :conversation_id).merge(conversation_id: @conversation.id))
     redirect_to "/conversations/#{@conversation.id}"
   end
 
   def show
     @conversation = Conversation.find(params[:id])
-    if Entry.where(:user_id => current_user.id, :conversation_id => @conversation.id).present?
+    if Entry.where(user_id: current_user.id, conversation_id: @conversation.id).present?
       @messages = @conversation.messages
       @message = Message.new
       @entries = @conversation.entries
