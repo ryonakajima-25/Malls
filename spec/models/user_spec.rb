0edfd1360525925ea2d26d3bde4e9337d39c4b87
ryonakajima-25 <nov.5x5@gmail.com>
2020-02-19 19:18:52 +0900

    テストコード記述（未完）

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,40 @@
+require 'rails_helper'
+
+describe User do
+  describe '#create' do
+    # 1. nameとemail、passwordとpassword_confirmationが存在すれば登録できること
+    it "is valid with a name, email, password, password_confirmation" do
+      user = build(:users)
+      expect(user).to be_valid
+    end
+
+    # 2. nameが空では登録できないこと
+    it "is invalid without a name" do
+      user = build(:user, name: nil)
+      user.valid?
+      expect(user.errors[:name]).to include("can't be blank")
+    end
+
+    # 3. emailが空では登録できないこと
+    it "is invalid without a email" do
+      user = build(:user, email: nil)
+      user.valid?
+      expect(user.errors[:email]).to include("can't be blank")
+    end
+
+    # 4. passwordが空では登録できないこと
+    it "is invalid without a password" do
+      user = build(:user, password: nil)
+      user.valid?
+      expect(user.errors[:password]).to include("can't be blank")
+    end
+
+    # 5. 重複したemailが存在する場合登録できないこと
+    it "is invalid with a duplicate email address" do
+      user = create(:user)
+      another_user = build(:user, email: user.email)
+      another_user.valid?
+      expect(another_user.errors[:email]).to include("has already been taken")
+    end
+  end
+end
